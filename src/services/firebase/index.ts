61e55d75fbd73551b9bade4f7e067a77a6cb7e9b
Parth Ahuja <parth.a@appdeskservices.com>
2024-01-10 17:30:50 +0530

    init

---
+++ src/services/firebase/index.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBAm6aiIQLGQ88J59gH9uB2RWcq9_iYhlo",
+  authDomain: "forms-a7859.firebaseapp.com",
+  projectId: "forms-a7859",
+  storageBucket: "forms-a7859.appspot.com",
+  messagingSenderId: "551124289631",
+  appId: "1:551124289631:web:daec509b38ac159fcff3ea",
+  measurementId: "G-MVVJ23KYK0"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
